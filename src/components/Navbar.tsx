94aa4ee2dd0087f8840177214078a711c6f24344
Shariful Islam <sajukhan12905@gmail.com>
2025-01-08 18:22:41 +0600

    make the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,12 @@ import React, { useState } from "react";
 import { RiMenu3Fill } from "react-icons/ri";
 import Logo from "../../public/logo-black.png";
 import navLinks from "@/constants/NavConstants";
-import { IoSearch } from "react-icons/io5";
+import { IoCloseSharp, IoSearch } from "react-icons/io5";
 import TextStyle from "./TextStyle";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   return (
     <nav className="  py-1 sticky top-0 z-50">
@@ -20,28 +21,74 @@ const Navbar = () => {
         {/* Desktop Menu */}
         <ul className="hidden lg:flex flex-1 ml-36 mr-44 justify-between space-x-8 items-center">
           {navLinks.map((item, index) => (
-            <li key={index} className="mx-4 hover:text-primary transition">
+            <li
+              key={index}
+              className="mx-4 hover:text-primary transition relative group"
+            >
               <Link href={item.link} className="font-semibold uppercase">
                 <TextStyle text={item.title} />
               </Link>
+              {item.navDropDown && (
+                <div className="absolute left-0 top-full  hidden group-hover:block bg-gray-900 text-white shadow-lg rounded-lg">
+                  <ul className="py-2 flex space-x-60">
+                    {item.navDropDown.map((subItem, subIndex) => (
+                      <li key={subIndex} className="px-4 py-2  ">
+                        <button className="font-semibold text-xl">
+                          {subItem.titleToggle}
+                        </button>
+                        <div>
+                          {subItem.items.map(
+                            (subMenuItem, subMenuItemIndex) => (
+                              <div
+                                key={subMenuItemIndex}
+                                className="px-4 py-2 w-max"
+                              >
+                                <Link href={subMenuItem.link}>
+                                  {subMenuItem.name}
+                                </Link>
+                              </div>
+                            )
+                          )}
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </li>
           ))}
-          <TextStyle text="Contact" />
         </ul>
 
         {/* Navbar right */}
 
-        <div className="flex items-center space-x-12 text-xl">
-          <div className="flex flex-col ">
-            <IoSearch />
-            <div className="p-4 absolute z-[-1] top-20 right-20 transform translate-y-12 opacity-0  rounded-lg bg-gray-200">
+        <div className="flex items-center space-x-12  text-xl">
+          <div className="flex flex-col relative">
+            <button>
+              {isSearchOpen ? (
+                <IoCloseSharp
+                  onClick={() => setIsSearchOpen(!isSearchOpen)}
+                  className="cursor-pointer"
+                />
+              ) : (
+                <IoSearch
+                  onClick={() => setIsSearchOpen(!isSearchOpen)}
+                  className="cursor-pointer"
+                />
+              )}
+            </button>
+
+
+           {isSearchOpen && (
+             <div className="p-4 absolute   right-0 transform translate-y-12  rounded-lg bg-gray-200">
               <input
                 type="text"
                 placeholder="Search"
                 className="  rounded-md p-2 text-lg w-72"
               />
             </div>
+            )}
           </div>
+
           <div className=" cursor-pointer " onClick={() => setIsOpen(!isOpen)}>
             <RiMenu3Fill />
           </div>
--- src/components/TextStyle.tsx
+++ src/components/TextStyle.tsx
@@ -1,21 +1,17 @@
-import React from "react";
-import "./TextStyled.css"; // Assuming you save the styles in a separate CSS file
+"use client";
 
-const TextStyle =  ({ text }: { text: string }) => {
-  const characters = text.split("");
+const TextStyle = ({ text }: { text: string }) => {
 
   return (
-    <button>
-      <div className="text">
-        {characters.map((char, index) => (
-          <span key={index}>{char}</span>
-        ))}
-        
-      </div>
-      <div className="clone">
-        {characters.map((char, index) => (
-          <span key={index}>{char}</span>
-        ))}
+    /* From Uiverse.io by Rauliqbal */
+    <button className="cursor-pointer  px-6 py-3 rounded-xl  border-none font-medium group">
+      <div className="relative overflow-hidden">
+        <p className="group-hover:-translate-y-7 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]">
+        {text}
+        </p>
+        <p className="absolute top-7 left-0 group-hover:top-0 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]">
+          {text}
+        </p>
       </div>
     </button>
   );
